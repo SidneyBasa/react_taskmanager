990a6b6bf56d926575cefe24ae19b69ee0c5e8cd
Sidney Basa <basa206@hotmail.com>
2023-09-02 00:03:01 -0700

    Added cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import React from 'react'
 import "./App.css"
 import TaskForm from './components/TaskForm'
 import TaskColumn from './components/TaskColumn'
+import todoIcon from './assets/direct-hit.png'
+import doingIcon from './assets/glowing-star.png'
+import doneIcon from './assets/check-mark-button.png'
 
 const App = () => {
   return (
     <div className='app'>
       <TaskForm/>
       <main className='app_main'>
-        <TaskColumn />
-      <section className='task_column'>Section 2</section>
-      <section className='task_column'>Section 3</section>
+        <TaskColumn title="To do" icon={todoIcon} />
+        <TaskColumn title="Doing" icon={doingIcon} />
+        <TaskColumn title="Done" icon={doneIcon}/>
+      
       </main>
     </div>
   )
--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Todo from "../assets/direct-hit.png"
 
 import "./TaskColumn.css"
+import TaskCard from './TaskCard'
 
-const TaskColumn = () => {
-    console.log(Todo)
+const TaskColumn = ({title, icon}) => {
   return (
     <section className='task_column'>
         <h2 className='task_column_heading'> 
-            <img className="task_column_icon" src={Todo} alt='' /> To Do
+            <img className="task_column_icon" src={icon} alt='' /> {title}
         </h2>
+        <TaskCard />
     </section>
   )
 }
