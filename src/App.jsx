51736d649a3b79827974b75fd5012df27877839a
Sidney Basa <basa206@hotmail.com>
2023-09-02 12:29:05 -0700

    Added interactive tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import "./App.css"
 import TaskForm from './components/TaskForm'
@@ -8,14 +8,16 @@ import doingIcon from './assets/glowing-star.png'
 import doneIcon from './assets/check-mark-button.png'
 
 const App = () => {
+  const [tasks, setTasks] = useState([])
+
+  console.log("tasks", tasks);
   return (
     <div className='app'>
-      <TaskForm/>
+      <TaskForm setTasks={setTasks}/>
       <main className='app_main'>
-        <TaskColumn title="To do" icon={todoIcon} />
-        <TaskColumn title="Doing" icon={doingIcon} />
-        <TaskColumn title="Done" icon={doneIcon}/>
-      
+        <TaskColumn title="To do" icon={todoIcon} tasks={tasks} status="todo" />
+        <TaskColumn title="Doing" icon={doingIcon} tasks={tasks} status="doing"/>
+        <TaskColumn title="Done" icon={doneIcon} tasks={tasks} status="done"/>
       </main>
     </div>
   )
