03ac4f09068f2969b6af8722518132031aafcca9
Sidney Basa <basa206@hotmail.com>
2023-09-01 23:27:45 -0700

    updated styling

---
+++ src/components/TaskColumn.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Todo from "../assets/direct-hit.png"
+
+import "./TaskColumn.css"
+
+const TaskColumn = () => {
+    console.log(Todo)
+  return (
+    <section className='task_column'>
+        <h2 className='task_column_heading'> 
+            <img className="task_column_icon" src={Todo} alt='' /> To Do
+        </h2>
+    </section>
+  )
+}
+
+export default TaskColumn
